0547c4aaba1986ff7d0b4518c202e70330ceb242
Jesse Millar <hellojessemillar@gmail.com>
2016-07-29 09:53:07 -0600

    Adding an environment variable to disable JWT checking on touchpanels

--- tools/miscellaneous/pi-touchpanel-setup.sh
+++ tools/miscellaneous/pi-touchpanel-setup.sh
@@ -8,8 +8,8 @@ docker pull byuoitav/rpi-telnet-microservice:latest
 docker pull byuoitav/rpi-pjlink-service:latest
 docker pull byuoitav/rpi-sony-control-microservice:latest
 
-docker run --restart=always -d -p 80:80 byuoitav/raspi-tp:latest
-docker run -e EMS_API_USERNAME=$EMS_API_USERNAME -e EMS_API_PASSWORD=$EMS_API_PASSWORD -d --restart=always --name av-api -p 8000:8000 byuoitav/rpi-av-api:latest
-docker run -d --restart=always --name telnet-microservice -p 8001:8001 byuoitav/rpi-telnet-microservice:latest
-docker run -d --restart=always --name pjlink-service -p 8005:8005 byuoitav/rpi-pjlink-service:latest
-docker run -e SONY_TV_PSK=$SONY_TV_PSK -d --restart=always --name sony-control-microservice -p 8007:8007 byuoitav/rpi-sony-control-microservice:latest
+docker run -e PI_TOUCHPANEL="true" --restart=always -d -p 80:80 byuoitav/raspi-tp:latest
+docker run -e PI_TOUCHPANEL="true" -e EMS_API_USERNAME=$EMS_API_USERNAME -e EMS_API_PASSWORD=$EMS_API_PASSWORD -d --restart=always --name av-api -p 8000:8000 byuoitav/rpi-av-api:latest
+docker run -e PI_TOUCHPANEL="true" -d --restart=always --name telnet-microservice -p 8001:8001 byuoitav/rpi-telnet-microservice:latest
+docker run -e PI_TOUCHPANEL="true" -d --restart=always --name pjlink-service -p 8005:8005 byuoitav/rpi-pjlink-service:latest
+docker run -e PI_TOUCHPANEL="true" -e SONY_TV_PSK=$SONY_TV_PSK -d --restart=always --name sony-control-microservice -p 8007:8007 byuoitav/rpi-sony-control-microservice:latest
