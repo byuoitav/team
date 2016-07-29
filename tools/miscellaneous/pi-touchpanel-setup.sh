802df29b445db1309cf5e71a9dc05a64117c4e07
Jesse Millar <hellojessemillar@gmail.com>
2016-07-29 09:13:55 -0600

    Adding a pull command to the script to function as an updater

--- tools/miscellaneous/pi-touchpanel-setup.sh
+++ tools/miscellaneous/pi-touchpanel-setup.sh
@@ -2,6 +2,12 @@
 
 # Sets up local AV API Docker containers on a Raspberry Pi touchpanel (run after drydock.sh for best effect)
 
+docker pull byuoitav/raspi-tp:latest
+docker pull byuoitav/rpi-av-api:latest
+docker pull byuoitav/rpi-telnet-microservice:latest
+docker pull byuoitav/rpi-pjlink-service:latest
+docker pull byuoitav/rpi-sony-control-microservice:latest
+
 docker run --restart=always -d -p 80:80 byuoitav/raspi-tp:latest
 docker run -e EMS_API_USERNAME=$EMS_API_USERNAME -e EMS_API_PASSWORD=$EMS_API_PASSWORD -d --restart=always --name av-api -p 8000:8000 byuoitav/rpi-av-api:latest
 docker run -d --restart=always --name telnet-microservice -p 8001:8001 byuoitav/rpi-telnet-microservice:latest
